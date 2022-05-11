1e1438304cb818ca519865e0a015cd7bf6a39a28
1 <bondarenko-1994alenka@mail.ru>
2022-05-11 20:19:47 +0300

    main

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿
+Console.WriteLine("Введите для сравнения два числа");
+
+int number1=Convert.ToInt32(Console.ReadLine());
+int number2=Convert.ToInt32(Console.ReadLine());
+
+int max=number1;
+if (max<number2)
+    max=number2;
+Console.WriteLine("Максимальное число "+ max);
